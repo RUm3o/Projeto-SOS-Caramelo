5785b505fbbd790487752ac4fd231847b913b36a
Reine Reis Umeo <reinerumeo@gmail.com>
2023-10-05 21:47:58 -0300

    atualização-front

---
+++ frontend/SOS/src/layouts/MainLayout.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://w7.pngwing.com/pngs/372/288/png-transparent-shiba-inu-dogecoin-internet-meme-others-face-carnivoran-dog-like-mammal-thumbnail.png">
+          </q-avatar>
+          SOS CARAMELO
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Registrar Nova Denuncia" />
+        <q-route-tab to="/page2" label="Acompanhe Denuncias em Andamento" />
+        <q-route-tab to="/page3" label="Crie Seu Login" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay elevated>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal elevated class="bg-brown-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/010/892/350/small/dog-paw-free-png.png">
+          </q-avatar>
+          <div>Projeto SOS Caramelo</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ frontend/SOS/src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Pets Logo"
+      src="https://www.seekpng.com/png/detail/868-8686204_pet-rules-cute-pets-together.png"
+      style="width: 860px; height: 388px; padding-bottom:"
+      
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
